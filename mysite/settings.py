a225885eb38c857883e4640adb8c744712ebc28d
Kenno-Warise <wariken0523@gmail.com>
2022-09-17 10:24:07 +0900

    heroku環境を削除して、シークレットキーの各種設定

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +22,29 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m)=g&92!4=5hvhipiu_d3fj&nypky*_(2#y&di*d4v#vdq!x^%'
+
+try:
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+except AttributeError:
+    import json
+
+    with open('secret_keys.json', 'r') as key:
+        secret_key = json.load(key)
+    SECRET_KEY = secret_key['DJANGO_SECRET_KEY']
+
+# SECRET_KEY = 'django-insecure-m)=g&92!4=5hvhipiu_d3fj&nypky*_(2#y&di*d4v#vdq!x^%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# 仮想環境のディレクトリが存在していればDEBUG=True
 
-ALLOWED_HOSTS = []
+path = 'venv' # 仮想環境のディレクトリ
+
+if not os.path.isdir(path):
+    DEBUG = False
+else:
+    DEBUG = True
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,19 +143,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-"""
-# Heroku設定
-
-import django_on_heroku
-
-django_on_heroku.settings(locals())
-
-# Set DEBUG to False in remote
-
-import os
-
-path = 'venv'
-
-if not os.path.isdir(path):
-    DEBUG = False
-"""
